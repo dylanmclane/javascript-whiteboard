0703efd6a222f7cb8211b2b6806ab587b6a3d84f
Dylan McLane <45742229+dylanmclane@users.noreply.github.com>
2023-05-11 01:39:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,14 +15,34 @@ var whiteboard = {
 		this.canvas.addEventListener("mousemove", this.draw.bind(this));
 		this.canvas.addEventListener("mouseup", this.endDraw.bind(this));
 		this.canvas.addEventListener("touchstart", this.startDraw.bind(this));
-		this.canvas.addEventListener("touchend", this.endDraw.bind(this));
 		this.canvas.addEventListener("touchmove", this.draw.bind(this));
+		this.canvas.addEventListener("touchend", this.endDraw.bind(this));
 		
 		// Attach event listener to the clear button
 		document.getElementById("clear-btn").addEventListener("click", this.clear.bind(this));
 		
 		// Attach event listener to the save button
 		document.getElementById("save-btn").addEventListener("click", this.save.bind(this));
+		
+		// Set up responsive canvas and buttons
+		this.setCanvasSize();
+		window.addEventListener("resize", this.setCanvasSize.bind(this));
+	},
+	
+	// Set the size of the canvas and buttons based on the window width
+	setCanvasSize: function() {
+		var width = Math.min(window.innerWidth * 0.9, 500);
+		var height = Math.min(window.innerHeight * 0.6, 500);
+		
+		this.canvas.width = width;
+		this.canvas.height = height;
+		
+		var buttons = document.querySelectorAll("button");
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].style.fontSize = Math.min(window.innerWidth * 0.03, 16) + "px";
+			buttons[i].style.padding = Math.min(window.innerWidth * 0.01, 10) + "px";
+			buttons[i].style.margin = Math.min(window.innerWidth * 0.01, 10) + "px";
+		}
 	},
 	
 	// Start drawing
@@ -30,27 +50,35 @@ var whiteboard = {
 		// Begin a new path on the canvas
 		this.ctx.beginPath();
 		
-		// Move the starting point to the current mouse position
-		var touch = event.touches[0];
-		this.ctx.moveTo(event.clientX - this.canvas.offsetLeft, event.clientY - this.canvas.offsetTop);
+		// Move the starting point to the current position
+		var x, y;
+		if (event.type === "mousedown") {
+			x = event.clientX - this.canvas.offsetLeft;
+			y = event.clientY - this.canvas.offsetTop;
+		} else if (event.type === "touchstart") {
+			var touch = event.touches[0];
+			x = touch.clientX - this.canvas.offsetLeft;
+			y = touch.clientY - this.canvas.offsetTop;
+		}
+		this.ctx.moveTo(x, y);
 	},
 	
 	// Draw
 	draw: function(event) {
-		// Check if the mouse button is being held down
-		if (event.buttons !== 1) {
-			return;
-		}
-		
-		// Draw a line to the current mouse position
-		this.ctx.lineTo(event.clientX - this.canvas.offsetLeft, event.clientY - this.canvas.offsetTop);
-
 		// Prevent scrolling on the canvas
 		event.preventDefault();
 		
-		// Draw a line to the current touch position
-		var touch = event.touches[0];
-		this.ctx.lineTo(touch.clientX - this.canvas.offsetLeft, touch.clientY - this.canvas.offsetTop);
+		// Draw a line to the current position
+		var x, y;
+		if (event.type === "mousemove") {
+			x = event.clientX - this.canvas.offsetLeft;
+			y = event.clientY - this.canvas.offsetTop;
+		} else if (event.type === "touchmove") {
+			var touch = event.touches[0];
+			x = touch.clientX - this.canvas.offsetLeft;
+			y = touch.clientY - this.canvas.offsetTop;
+		}
+		this.ctx.lineTo(x, y);
 		
 		// Set the stroke style and width
 		this.ctx.strokeStyle = "#000";
@@ -64,19 +92,17 @@ var whiteboard = {
 	endDraw: function(event) {
 		// End the path
 		this.ctx.closePath();
-	},
-	
+		},
 	// Clear the canvas
 	clear: function() {
+		// Clear the canvas
 		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 	},
 	
-	// Save the canvas as an image
+	// Save the image
 	save: function() {
-		var link = document.createElement("a");
-		link.download = "whiteboard.png";
-		link.href = this.canvas.toDataURL();
-		link.click();
+		// Open the image in a new window
+		window.open(this.canvas.toDataURL("image/png"));
 	}
 };
 
